c06b2d39a1212e6cffff93d419f8de6c857ff99a
Xuân Quý <81242756+xqsadness@users.noreply.github.com>
2024-06-14 15:50:00 +0700

    init

---
+++ ChatWithGemini/ChatWithGeminiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithGeminiApp.swift
+//  ChatWithGemini
+//
+//  Created by xqsadness on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithGeminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatWithGemini/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChatWithGemini
+//
+//  Created by xqsadness on 14/06/2024.
+//
+
+import SwiftUI
+import GoogleGenerativeAI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           
+        }
+        .padding()
+    }
+}
+#Preview {
+    ContentView()
+}
